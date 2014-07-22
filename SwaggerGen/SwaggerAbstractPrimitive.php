b7eda67bc65594c5f4144d1a677cf01491aa1500
Martijn van der Lee <martijn@crmmailtech.nl>
2014-07-22 21:14:22 +0200

    Support for enum primitive

--- SwaggerGen/SwaggerAbstractPrimitive.php
+++ SwaggerGen/SwaggerAbstractPrimitive.php
@@ -5,82 +5,27 @@ abstract class SwaggerAbstractPrimitive extends SwaggerAbstractScope {
 	public $default;
 	public $enum;
 	public $minimum;
-	public $maximum;	
+	public $maximum;
 	public $items;
 	public $uniqueitems;
 
 	private static $primitives = array(
-		'integer' => array(
-			'type' => 'integer',
-			'format' => 'int32',
-			'items' => 'integer',
-		),
-		'long' => array(
-			'type' => 'integer',
-			'format' => 'int64',
-			'items' => 'integer',
-		),
-		'float' => array(
-			'type' => 'number',
-			'format' => 'float',
-			'items' => 'number',
-		),
-		'double' => array(
-			'type' => 'number',
-			'format' => 'double',
-			'items' => 'number',
-		),
-		'string' => array(
-			'type' => 'string',
-			'format' => '',
-			'items' => 'string',
-		),
-		'byte' => array(
-			'type' => 'string',
-			'format' => 'byte',
-			'items' => 'string',
-		),
-		'boolean' => array(
-			'type' => 'boolean',
-			'format' => '',
-			'items' => 'boolean',
-		),
-		'date' => array(
-			'type' => 'string',
-			'format' => 'date',
-			'items' => 'string',
-		),
-		'datetime' => array(
-			'type' => 'string',
-			'format' => 'date-time',
-			'items' => 'string',
-		),
-		'array' => array(
-			'type' => 'array',
-			'format' => '',
-			'items' =>
-			NULL,
-		),
-		'set' => array(
-			'type' => 'array',
-			'format' => 'set',
-			'items' =>
-			NULL,
-		),
-		'file' => array(
-			'type' => 'File',
-			'format' =>
-			NULL,
-			'items' =>
-			NULL,
-		),
-		'void' => array(
-			'type' => 'void',
-			'format' =>
-			NULL,
-			'items' =>
-			NULL,
-		),
+		'integer' => array('type' => 'integer','format' => 'int32','items' => 'integer'),
+		'long' => array('type' => 'integer','format' => 'int64','items' => 'integer'),
+		'float' => array('type' => 'number','format' => 'float','items' => 'number'),
+		'double' => array('type' => 'number','format' => 'double','items' => 'number'),
+		'string' => array('type' => 'string','format' => '','items' => 'string'),
+		'byte' => array('type' => 'string','format' => 'byte','items' => 'string'),
+		'boolean' => array('type' => 'boolean','format' => '','items' => 'boolean'),
+		'date' => array('type' => 'string','format' => 'date','items' => 'string'),
+		'datetime' => array('type' => 'string','format' => 'date-time','items' => 'string'),
+		'array' => array('type' => 'array',	'format' => '',	'items' =>	NULL),
+		'set' => array('type' => 'array', 'format' => 'set', 'items' =>	NULL),
+		'file' => array('type' => 'File', 'format' => NULL, 'items' => NULL),
+		'void' => array('type' => 'void', 'format' => NULL, 'items' => NULL),
+
+		// Non-standard convenience types
+		'enum' => array('type' => 'string', 'format' => '', 'items' => 'string'),
 	);
 
 	public function __construct(SwaggerAbstractBase $Base, $primitive) {
@@ -88,23 +33,25 @@ abstract class SwaggerAbstractPrimitive extends SwaggerAbstractScope {
 
 		$this->setPrimitive($primitive);
 	}
-	
+
 	private function setPrimitive($primitive) {
 		if (preg_match('~^(\w+)\\(([^,)]*),?([^)]*)\\)$~', $primitive, $match) === 1) {
 			$this->type		= $match[1];
-			
+
 			if (in_array($match[1], array('array', 'set'))) {
-				$this->items = $match[2];				
+				$this->items = $match[2];
 				if ($match[1] === 'set') {
 					$this->uniqueitems = true;
 				}
+			} else if ($match[1] === 'enum') {
+				$this->enum = explode(',', $match[2].','.$match[3]);
 			} else if (isset($match[2])) {
 				$this->minimum = $match[2];
 				$this->maximum = $match[empty($match[3])? 2 : 3];
 			}
-		} else {		
+		} else {
 			$this->type		= $primitive;
-		}		
+		}
 	}
 
 	private function getType() {
@@ -120,7 +67,7 @@ abstract class SwaggerAbstractPrimitive extends SwaggerAbstractScope {
 		}
 		return null;
 	}
-	
+
 	private function getItems() {
 		if (isset(self::$primitives[strtolower($this->items)]['items'])) {
 			return self::$primitives[strtolower($this->items)]['items'];
@@ -139,7 +86,7 @@ abstract class SwaggerAbstractPrimitive extends SwaggerAbstractScope {
 			'enum'			=> $this->enum,
 			'minimum'		=> $this->minimum,
 			'maximum'		=> $this->maximum,
-			'items'			=> $this->getItems() ?: $this->items,	
+			'items'			=> $this->getItems() ?: $this->items,
 			'uniqueItems'	=> $this->uniqueitems,
 		);
 
