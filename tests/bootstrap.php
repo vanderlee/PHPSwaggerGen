91fcff5c737ec735aee4d960d4dd0e1986c30b22
Martijn <martijn@vanderlee.com>
2015-11-20 20:59:50 +0100

    Relative path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,7 @@
 <?php
 
 	spl_autoload_register(function ($classname) {
-		$file = __DIR__ . '/../' . $classname . '.php';
-		var_dump($file);
+		$file = dirname(__DIR__) . '/' . $classname . '.php';
 		if (is_file($file)) {
 			include_once $file;
 		}
