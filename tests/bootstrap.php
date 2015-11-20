b6e178d78fdc8feb1135857fcc459beaa5facfac
Martijn <martijn@vanderlee.com>
2015-11-20 22:01:59 +0100

    require

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,6 @@
 	spl_autoload_register(function ($classname) {
 		$file = dirname(__DIR__) . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $classname) . '.php';
 		if (is_file($file)) {
-			include_once $file;
+			require_once $file;
 		}
 	});
