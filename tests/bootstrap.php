b29a9db2b3521a19241368d2d2a9e6b9591c2c2d
Martijn <martijn@vanderlee.com>
2015-11-20 20:57:24 +0100

    Debug

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,8 @@
 <?php
 
 	spl_autoload_register(function ($classname) {
-		$file = __DIR__ . '/../' . $classname . '.php';
+		$file = __DIR__ . '/' . $classname . '.php';
+		var_dump(__DIR__);
 		if (is_file($file)) {
 			include_once $file;
 		}
