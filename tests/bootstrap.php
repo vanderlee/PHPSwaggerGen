21cc60889f33d732d31ec9e8596eb71506afbe1b
Martijn <martijn@vanderlee.com>
2015-11-20 21:57:58 +0100

    Path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,7 @@
 <?php
 
 	spl_autoload_register(function ($classname) {
-		$file = dirname(__DIR__) . '/' . $classname . '.php';
-		var_dump(glob('*'));
+		$file = dirname(__DIR__) . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $classname) . '.php';
 		if (is_file($file)) {
 			include_once $file;
 		}
