d1eb6bc69f99171deb4cf41664383c4b8aaf0441
Martijn <martijn@vanderlee.com>
2015-12-20 16:27:10 +0100

    PHP statement

---
+++ SwaggerGen/Parser/Php/Statement.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SwaggerGen\Parser\Php;
+
+/**
+ * Represents a PHP-specific command statement, keeping track of context for
+ * debugging purposes.
+ *
+ * @package    SwaggerGen
+ * @author     Martijn van der Lee <martijn@vanderlee.com>
+ * @copyright  2014-2015 Martijn van der Lee
+ * @license    https://opensource.org/licenses/MIT MIT
+ */
+class Statement extends \SwaggerGen\Statement
+{
+
+	public $command;
+	public $data;
+	public $file;
+	public $line;
+
+	public function __construct($command, $data, $file, $line)
+	{
+		$this->command = $command;
+		$this->data = $data;
+		$this->file = $file;
+		$this->line = $line;
+	}
+
+	public function __toString()
+	{
+		$message = parent::__toString();
+		$message .= " in line {$this->line}";
+		$message .= $this->file ? " of file '{$this->file}'." : ' of code string';
+		return $message;
+	}
+
+}
