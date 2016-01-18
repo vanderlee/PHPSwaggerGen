1257e4e04527cab4a30ceb0190ef28f39a00392c
Martijn <martijn@vanderlee.com>
2016-01-18 20:05:52 +0100

    Added Header tests

---
+++ tests/Swagger/HeaderTest.php
@@ -0,0 +1,75 @@
+<?php
+
+class HeaderTest extends PHPUnit_Framework_TestCase
+{
+
+	protected $parent;
+
+	protected function setUp()
+	{
+		$this->parent = $this->getMockForAbstractClass('\SwaggerGen\Swagger\AbstractObject');
+	}
+
+	protected function assertPreConditions()
+	{
+		$this->assertInstanceOf('\SwaggerGen\Swagger\AbstractObject', $this->parent);
+	}
+
+	/**
+	 * @covers \SwaggerGen\Swagger\Header::__construct
+	 */
+	public function testConstructorType()
+	{
+		$object = new \SwaggerGen\Swagger\Header($this->parent, 'integer');
+
+		$this->assertInstanceOf('\SwaggerGen\Swagger\Header', $object);
+
+		$this->assertSame(array(
+			'type' => 'integer',
+				), $object->toArray());
+	}
+
+	/**
+	 * @covers \SwaggerGen\Swagger\Header::__construct
+	 */
+	public function testConstructorInvalidType()
+	{
+		$this->setExpectedException(
+          '\SwaggerGen\Exception', 'Header type not valid: BadType'
+        );
+		
+		new \SwaggerGen\Swagger\Header($this->parent, 'BadType');
+	}
+
+	/**
+	 * @covers \SwaggerGen\Swagger\Header::__construct
+	 */
+	public function testConstructorFull()
+	{
+		$object = new \SwaggerGen\Swagger\Header($this->parent, 'integer', 'descriptive text');
+
+		$this->assertInstanceOf('\SwaggerGen\Swagger\Header', $object);
+
+		$this->assertSame(array(
+			'type' => 'integer',
+			'description' => 'descriptive text',
+				), $object->toArray());
+	}
+
+	/**
+	 * @covers \SwaggerGen\Swagger\Header->handleCommand
+	 */
+	public function testCommandDescription()
+	{
+		$object = new \SwaggerGen\Swagger\Header($this->parent, 'integer', 'descriptive text');
+
+		$this->assertInstanceOf('\SwaggerGen\Swagger\Header', $object);
+
+		$object->handleCommand('description', 'Some other lines');
+
+		$this->assertSame(array(
+			'type' => 'integer',
+			'description' => 'Some other lines',
+				), $object->toArray());
+	}
+}
