86540cc82733c9ae1963eadb2030c87ed3480024
Martijn <martijn@vanderlee.com>
2016-01-22 20:43:42 +0100

    Added ReferenceObjectType test

---
+++ tests/Swagger/Type/ReferenceObjectTypeTest.php
@@ -0,0 +1,33 @@
+<?php
+
+class ReferenceObjectTypeTest extends PHPUnit_Framework_TestCase
+{
+
+	protected $parent;
+
+	protected function setUp()
+	{
+		$this->parent = $this->getMockForAbstractClass('\SwaggerGen\Swagger\AbstractObject');
+	}
+
+	protected function assertPreConditions()
+	{
+		$this->assertInstanceOf('\SwaggerGen\Swagger\AbstractObject', $this->parent);
+	}
+
+	/**
+	 * @covers \SwaggerGen\Swagger\Type\ReferenceObjectType::__construct
+	 */
+	public function testConstructBothConsumes()
+	{
+		$object = new SwaggerGen\Swagger\Type\ReferenceObjectType($this->parent, 'blah');
+
+		$this->assertInstanceOf('\SwaggerGen\Swagger\Type\ReferenceObjectType', $object);
+
+		$this->assertSame(array(
+			'type' => 'object',
+			'$ref' => '#/definitions/blah',
+				), $object->toArray());
+	}
+
+}
