46f6cd23b5625427333c3d7484ad853474a1137c
Martijn <martijn@vanderlee.com>
2016-01-22 20:06:44 +0100

    Added AbstractType test

---
+++ tests/Swagger/Type/AbstractTypeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+class AbstractTypeTest extends PHPUnit_Framework_TestCase
+{
+
+	protected $parent;
+
+	protected function setUp()
+	{
+		$this->parent = $this->getMockForAbstractClass('\SwaggerGen\Swagger\AbstractObject');
+	}
+
+	protected function assertPreConditions()
+	{
+		$this->assertInstanceOf('\SwaggerGen\Swagger\AbstractObject', $this->parent);
+	}
+
+	/**
+	 * @covers \SwaggerGen\Swagger\AbstractType::__construct
+	 */
+	public function testConstruct()
+	{
+		$stub = $this->getMockForAbstractClass('SwaggerGen\Swagger\Type\AbstractType', array(
+			$this->parent,
+			'whatever'
+		));
+
+		$this->assertFalse($stub->handleCommand('x-extra', 'whatever'));
+	}
+
+}
