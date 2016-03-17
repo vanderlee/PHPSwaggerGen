20189485256df2715a8e58fe20d368064e76da3e
Martijn <martijn@vanderlee.com>
2016-03-17 20:01:04 +0100

    Some placeholders for classes without their own tests

---
+++ tests/Parser/IParserTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class Parser_IParserTest extends PHPUnit_Framework_TestCase
+{
+
+	public function testPlaceholder()
+	{
+		$this->markTestIncomplete('There are no tests here. This file is here for consistency.');
+	}
+
+}
