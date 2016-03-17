cc80616ccbe268bf2d81d333f45c3a55f8cfa319
Martijn <martijn@vanderlee.com>
2016-03-17 20:00:19 +0100

    Some placeholders for classes without their own tests

---
+++ tests/Swagger/IParameter.php
@@ -0,0 +1,11 @@
+<?php
+
+class Swagger_IParameterTest extends PHPUnit_Framework_TestCase
+{
+
+	public function testPlaceholder()
+	{
+		$this->markTestIncomplete('There are no tests here. This file is here for consistency.');
+	}
+
+}
