dd9ebbf1dff085f368840639af97e469aab2c307
Martijn <martijn@vanderlee.com>
2016-10-19 20:57:02 +0200

    PHP5.3 does not do pretty JSON; ugly JSON test which do not explicitely test pretty JSON.

--- tests/output/OutputTest.php
+++ tests/output/OutputTest.php
@@ -10,7 +10,7 @@ class OutputTest extends PHPUnit_Framework_TestCase
 	public function testAllCases($name, $files, $expected)
 	{
 		$SwaggerGen = new \SwaggerGen\SwaggerGen('example.com', '/base');
-		$actual = $SwaggerGen->getSwagger($files, array(), \SwaggerGen\SwaggerGen::FORMAT_JSON_PRETTY);
+		$actual = $SwaggerGen->getSwagger($files, array(), \SwaggerGen\SwaggerGen::FORMAT_JSON);
 		$this->assertSame($expected, $actual, $name);
 	}
 	
@@ -19,12 +19,13 @@ class OutputTest extends PHPUnit_Framework_TestCase
 		
 		foreach (glob(__DIR__ . '/*', GLOB_ONLYDIR) as $dir) {					
 			$path = realpath($dir);					
+			$json = json_encode(json_decode(file_get_contents($path . '/expected.json')));
 			$cases[] = array(
 				basename($dir),
 				array(
 					$path . '/source.php',
 				),
-				file_get_contents($path . '/expected.json')
+				$json
 			);
 		}
 		
