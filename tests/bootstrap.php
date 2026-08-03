7eb74c630d24dcd7b757471b80cefbf5671947f2
Martijn van der Lee <martijn@vanderlee.com>
2026-08-03 10:06:47 +0200

    Restore the PHPUnit test base alias

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!class_exists('SwaggerGen_TestCase', false)) {
+    class_alias('PHPUnit\\Framework\\TestCase', 'SwaggerGen_TestCase');
+}
+
 spl_autoload_register(function ($classname) {
     $file = dirname(__DIR__) . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $classname) . '.php';
     if (is_file($file)) {
